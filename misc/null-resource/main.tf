7abcec02d4edf20524befaeb60622461494a1765
Usman <usmanakramb@gmail.com>
2025-06-30 10:33:19 -0500

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "null" {
+resource "null_resource" "null1" {
   provisioner "local-exec" {
     command = "echo This command will execute whenever the configuration changes"
   }
