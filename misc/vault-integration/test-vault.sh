e99af376a2f4bb848d2a3ec77acf5245f4ba848c
Usman <usmanakramb@gmail.com>
2025-07-21 21:13:13 -0500

    Update test-vault.sh

--- misc/vault-integration/test-vault.sh
+++ misc/vault-integration/test-vault.sh
@@ -10,7 +10,7 @@ fi
 
 echo "Logging in to Vault"
 
-export VAULT_TOKEN=$(./vault write auth/jwt/login role="${VAULT_ROLE}" jwt="${ENV0_OIDC_TOKEN}" -format=json | jq --raw-output '.auth.client_token')
+#export VAULT_TOKEN=$(./vault write auth/jwt/login role="${VAULT_ROLE}" jwt="${ENV0_OIDC_TOKEN}" -format=json | jq --raw-output '.auth.client_token')
 
 echo $VAULT_TOKEN
 
