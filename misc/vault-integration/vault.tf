c0c3127d80b7e93d60af49b84b4f29965fd10bf2
Usman <usmanakramb@gmail.com>
2025-07-21 22:22:18 -0500

    Create vault.tf

---
+++ misc/vault-integration/vault.tf
@@ -0,0 +1,35 @@
+provider "vault" {
+  address          = "http://13.221.115.246:8200"
+  skip_child_token = true
+}
+
+terraform {
+  required_providers {
+    env0 = {
+      source = "env0/env0"
+      version = "1.29.3"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "4.7.0"
+  }
+}
+
+
+resource "env0_vault_oidc_credentials" "demo" {
+  name                  = "demo"
+  address               = "http://13.221.115.246:8200"
+  version               = "1.20.0"
+  role_name             = "vault_role"
+  jwt_auth_backend_path = "jwt"
+}
+
+resource "vault_generic_secret" "vault_example" {
+  path                = "secret/env0/test"
+  delete_all_versions = true
+  data_json           = <<EOT
+{
+  "foo":   "bar"
+}
+EOT
+}
