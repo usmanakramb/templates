2313b9ae078265d8dd21056f68fc6bdb3f891311
Usman <usmanakramb@gmail.com>
2025-07-21 22:50:08 -0500

    Update vault.tf

--- misc/vault-integration/vault.tf
+++ misc/vault-integration/vault.tf
@@ -37,12 +37,29 @@ resource "env0_vault_oidc_credentials" "demo" {
   jwt_auth_backend_path = "jwt"
 }
 
-resource "vault_generic_secret" "vault_example" {
-  path                = "secret/env0/test"
-  delete_all_versions = true
-  data_json           = <<EOT
-{
-  "foo":   "bar"
+resource "vault_mount" "kvv2" {
+  path        = "secret"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "KV Version 2 secret engine mount"
 }
-EOT
+
+resource "vault_kv_secret_v2" "example" {
+  mount                      = vault_mount.kvv2.path
+  name                       = "secret"
+  cas                        = 1
+  delete_all_versions        = true
+  data_json                  = jsonencode(
+  {
+    zip       = "zap",
+    foo       = "bar"
+  }
+  )
+  custom_metadata {
+    max_versions = 5
+    data = {
+      foo = "vault@example.com",
+      bar = "12345"
+    }
+  }
 }
